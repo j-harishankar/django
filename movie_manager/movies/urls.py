b37824230061ec0fd092b5ebdf563a7ac5132675
j harishankar <jharivichu27@gmail.com>
2025-06-11 19:07:58 +0530

    added static images

--- movie_manager/movies/urls.py
+++ movie_manager/movies/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('',views.create,name='create'),
     path('list/',views.list,name = 'list'),
     path('edit/',views.edit,name = 'edit'),
-
+    path('movie/',views.movie,name = 'movie')
 
 
 ]
--- movie_manager/movies/views.py
+++ movie_manager/movies/views.py
@@ -7,3 +7,21 @@ def list(request):
     return render(request,'list.html')
 def edit(request):
     return render(request,'edit.html')
+def movie(request):
+    movie_list ={'movies':
+    [
+    {
+        'title':"Godfather",
+        'year':"1994",
+        'summary':"story of an underworld king",
+        'image':'godfather.png'
+    },
+    {
+        'title':"batman",
+        'year':"2000",
+        'summary':"I am vengence",
+        'image':'batman.jpg'
+    }
+    ]
+    }
+    return render(request,'movie.html',movie_list)
