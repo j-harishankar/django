4ba5dd872900a0db2ccccf781f7410fccded2c21
j harishankar <jharivichu27@gmail.com>
2025-06-28 17:24:01 +0530

    Many to Many field added

--- movie_manager/movies/models.py
+++ movie_manager/movies/models.py
@@ -6,7 +6,8 @@ class director(models.Model):
 class CensorInfo(models.Model):
     rating = models.CharField(max_length=10)
     certified_by = models.CharField(max_length=200,null=True)
-
+class Actor(models.Model):
+    name = models.CharField(max_length=300)
 class MovieInfo(models.Model):
     title = models.CharField(max_length=250)
     year = models.IntegerField()
@@ -14,6 +15,7 @@ class MovieInfo(models.Model):
     poster = models.ImageField(upload_to='images/',null=True)
     censor = models.OneToOneField(CensorInfo,on_delete=models.SET_NULL,related_name='movie',null=True)
     directed_by = models.ForeignKey(director,on_delete=models.CASCADE,related_name='directed_movie',null=True)
+    actors = models.ManyToManyField(Actor,related_name="actor_movie")
     def __str__(self):
         return self.title
 # If you don’t override this method, Django uses the default string representation provided by Python,
